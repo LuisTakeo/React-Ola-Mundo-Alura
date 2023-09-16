186bae11848fd5e76aadd079abc0eae97dd342f9
LuisTakeo <luistakeo@hotmail.com>
2023-09-16 05:07:39 -0300

    Commit com alterações do sobre mim.

--- src/pages/SobreMim/index.js
+++ src/pages/SobreMim/index.js
@@ -2,43 +2,20 @@ import styles from "./SobreMim.module.css";
 
 import PostModelo from "components/PostModelo";
 import fotoCapa from "assets/sobre_mim_capa.png";
-import fotoSobreMim from "assets/sobre_mim_foto.png";
+import fotoSobreMim from "assets/minha_foto (2).png";
 
 export default function Sobremim() {
   return (
     <PostModelo fotoCapa={fotoCapa}>
-      <h3 className={styles.subtitulo}>Olá, meu nome é Antonio!</h3>
+      <h3 className={styles.subtitulo}>Olá, meu nome é LuisTakeo!</h3>
       <img src={fotoSobreMim} alt="" className={styles.fotoSobreMim} />
       <p>
-        Oi, tudo bem? Eu sou instrutor de Front-end na Alura e estou feliz de te
-        ver por aqui. Minha história com programação começou no Instituto
-        Federal do Piauí (IFPI), quando fiz o ensino médio integrado ao curso de
-        Informática. Eu aprendi lógica de programação e o básico de várias
-        linguagens, como PHP, Python, Java, mas sem aprofundar muito em cada uma
-        delas. Eu gostava muito de estudar programação, mas na época não fazia
-        ideia de que trabalharia com isso hoje. No ensino superior, escolhi
-        cursar Engenharia Elétrica na Universidade Federal do Piauí (UFPI). Lá
-        eu consegui entrar no PET (Programa de Educação Tutorial), um grupo
-        acadêmico que contribui de diversas formas para o curso. Lá eu tive a
-        oportunidade de estudar desenvolvimento web para ser um dos mantenedores
-        do site deles. No ensino superior, escolhi cursar Engenharia Elétrica na
-        Universidade Federal do Piauí (UFPI). Lá eu consegui entrar no PET
-        (Programa de Educação Tutorial), um grupo acadêmico que contribui de
-        diversas formas para o curso. Lá eu tive a oportunidade de estudar
-        desenvolvimento web para ser um dos mantenedores do site deles.
-      </p>
-      <p>
-        Com isso tive minha experiência de dev mais próxima da realidade, com
-        prazos de implementação para o site, e aprendi muito enquanto
-        codificava. E foi enquanto eu estava no PET que decidi procurar um
-        estágio em desenvolvimento web. Eu encontrei uma vaga na Alura para o
-        Scuba Team e percebi que ela aliava duas paixões minhas: programação e
-        educação. Para minha felicidade, fui escolhido para fazer parte do time.
-        Desde então, tem sido aprenas aprendizados atrás de aprendizados. A
-        Alura é uma escola não só para seus alunos e alunas, mas também para os
-        colaboradores e colaboradoras. Hoje sou muito feliz de ter a
-        oportunidade de trazer esses conteúdos ricos e encantadores para você.
-        Espero que aprenda bastante!
+        Já trabalhei na área de atendimento por anos, tanto por voz quanto via
+        chat/e-mail, e fui considerado como um dos melhores atendentes do meu
+        setor. Há 1 ano e meio fiz uma mudança a qual decidi entrar na área de
+        programação. Tenho muito interesse e gosto pela área de desenvolvimento
+        web. As tecnologias que tenho conhecimento: Angular, React, Typescript,
+        HTML - CSS - Javascript, Java, POO/OOP, C, MVC, PHP, Python, MySQL.
       </p>
     </PostModelo>
   );
